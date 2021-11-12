7de0bd598640948249ec707554e62085e4fbce2f
Gerardas Ciegis <gerardasc@gmail.com>
2021-11-12 17:50:46 +0200

    6: data fetch in component

--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -1,11 +1,36 @@
 class CarGridComponent {
   constructor() {
+    this.state = {
+      loading: false,
+      cars: []
+    }
     this.init();
   }
 
-  init = () => {
-    this.htmlElement = document.createElement('div');
-    this.htmlElement.innerHTML = 'Elementas';
+  fetchCars = () => API.fetchCars(this.saveCars, alert)
+
+  saveCars = (cars) => {
+    this.state.cars = cars;
+    this.state.loading = false;
+    console.log('1')
+    this.render();
   }
 
+  init = () => {
+    this.state.loading = true;
+    this.fetchCars();
+    this.htmlElement = document.createElement('div');
+    this.render();
+    console.log('2')
+  }
+
+  render = () => {
+    const { loading, cars } = this.state;
+
+    if (loading) {
+      this.htmlElement.innerHTML = 'siunciama...';
+    } else {
+      this.htmlElement.innerHTML = 'parsiusta'
+    }
+  }
 }
